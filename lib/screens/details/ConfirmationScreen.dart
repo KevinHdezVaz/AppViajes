d8738f60ad835be97c0a2848450f019e7e230ae4
Kevin Hernandez Vazquez <kevin.hernandez@caffenio.com>
2023-12-29 02:08:40 -0600

    update_reserva

--- lib/screens/details/ConfirmationScreen.dart
+++ lib/screens/details/ConfirmationScreen.dart
@@ -1,8 +1,10 @@
 import 'package:appviajes/models/SpotData.dart';
+import 'package:dotted_border/dotted_border.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_staggered_animations/flutter_staggered_animations.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'package:intl/intl.dart';
+import 'package:lottie/lottie.dart';
 
 class ConfirmationScreen extends StatefulWidget {
   final String title;
@@ -47,6 +49,51 @@ List<Map<String, dynamic>> filteredHotels = [];
   }
 
  
+
+void _showPackageDetails(BuildContext context) {
+  showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return AlertDialog(
+        title: Text('Detalles del Paquete'),
+        content: SingleChildScrollView(
+          child: ListBody(
+            children: widget.packages.map((package) {
+              return Padding(
+                padding: const EdgeInsets.symmetric(vertical: 8.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      package['name'],
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    ...package['details'].map<Widget>((detail) {
+                      return Text('• $detail');
+                    }).toList(),
+                    SizedBox(height: 10),
+                    Text('Precios:'),
+                    ...package['prices'].entries.map<Widget>((entry) {
+                      return Text('${entry.key}: \$${entry.value}');
+                    }).toList(),
+                  ],
+                ),
+              );
+            }).toList(),
+          ),
+        ),
+        actions: <Widget>[
+          TextButton(
+            child: Text('Cerrar'),
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+          ),
+        ],
+      );
+    },
+  );
+}
 
 
 void _filterHotels(String query) {
@@ -182,7 +229,10 @@ Future<void> _showHotelSelectionModal(BuildContext context) async {
         title: Text(widget.title),
       ),
       body: SingleChildScrollView(
+
+        
         child: AnimationLimiter(
+          
           child: Column(
             children: AnimationConfiguration.toStaggeredList(
               duration: const Duration(milliseconds: 1375),
@@ -193,73 +243,169 @@ Future<void> _showHotelSelectionModal(BuildContext context) async {
                 ),
               ),
               children: [
-                Card(
-                  child: ListTile(
-                    title: Text(isDateSelected
-                        ? 'Fecha seleccionada: ${DateFormat('yyyy-MM-dd').format(selectedDate)}'
-                        : 'Selecciona una fecha'),
-                    trailing: Icon(Icons.calendar_today),
-                    onTap: () => _selectDate(context),
-                  ),
-                ),
-                Card(
-                  child: ListTile(
-                    title: Text('Número de Pasajeros'),
-                    trailing: DropdownButton<int>(
-                      value: numberOfPassengers,
-                      onChanged: (int? newValue) {
-                        setState(() {
-                          numberOfPassengers = newValue ?? 1;
-                          _calculateTotal();
-                        });
-                      },
-                      items: List.generate(10, (index) => index + 1)
-                          .map<DropdownMenuItem<int>>((int value) {
-                        return DropdownMenuItem<int>(
-                          value: value,
-                          child: Text(value.toString()),
-                        );
-                      }).toList(),
+                 Column(
+                      children: [
+                        Lottie.asset(
+                          'assets/animacion/reserva.json',
+                          fit: BoxFit.cover,
+                          width: 200, // Establece el ancho deseado
+                          height: 200, // Establece el alto deseado
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: 26),
+                Padding(
+                  padding: const EdgeInsets.only(left: 20, right: 20),
+                  child: Card(
+                    child: ListTile(
+                      title: Text(isDateSelected
+                          ? 'Fecha seleccionada: ${DateFormat('yyyy-MM-dd').format(selectedDate)}'
+                          : 'Selecciona una fecha'),
+                      trailing: Icon(Icons.calendar_today),
+                      onTap: () => _selectDate(context),
                     ),
                   ),
                 ),
-                Card(
-                  child: ListTile(
-                    title: Text('Seleccionar Paquete'),
-                    trailing: DropdownButton<String>(
-                      value: selectedPackage,
-                      onChanged: (String? newValue) {
-                        setState(() {
-                          selectedPackage = newValue;
-                          _calculateTotal();
-                        });
-                      },
-                      items: widget.packages
-                          .map<DropdownMenuItem<String>>((package) {
-                        return DropdownMenuItem<String>(
-                          value: package['name'],
-                          child: Text(package['name']),
-                        );
-                      }).toList(),
-                      hint: selectedPackage == null
-                          ? Text('Seleccione un paquete')
-                          : null,
+                SizedBox(height: 26),
+                Padding(
+    padding: const EdgeInsets.only(left: 20, right: 20),
+                      child: Card(
+                    child: ListTile(
+                      title: Text('Número de Pasajeros'),
+                      trailing: DropdownButton<int>(
+                        value: numberOfPassengers,
+                        onChanged: (int? newValue) {
+                          setState(() {
+                            numberOfPassengers = newValue ?? 1;
+                            _calculateTotal();
+                          });
+                        },
+                        items: List.generate(10, (index) => index + 1)
+                            .map<DropdownMenuItem<int>>((int value) {
+                          return DropdownMenuItem<int>(
+                            value: value,
+                            child: Text(value.toString()),
+                          );
+                        }).toList(),
+                      ),
                     ),
                   ),
                 ),
+                SizedBox(height: 26),
+                Padding(
+                 padding: const EdgeInsets.only(left: 20, right: 20),        
+              child: Card(
+                    child: ListTile(
+  title: Text('Seleccionar Paquete'),
+  trailing: Row(
+    mainAxisSize: MainAxisSize.min,
+    children: [
+      DropdownButton<String>(
+        value: selectedPackage,
+        onChanged: (String? newValue) {
+          setState(() {
+            selectedPackage = newValue;
+            _calculateTotal();
+          });
+        },
+        items: widget.packages
+            .map<DropdownMenuItem<String>>((package) {
+          return DropdownMenuItem<String>(
+            value: package['name'],
+            child: Text(package['name']),
+          );
+        }).toList(),
+        hint: selectedPackage == null
+            ? Text('Seleccione un paquete')
+            : null,
+      ),
+      IconButton(
+        icon: Icon(Icons.help_outline),
+        onPressed: () => _showPackageDetails(context),
+      ),
+    ],
+  ),
+)
+
+                  ),
+                ),SizedBox(height: 26),
            // Reemplaza este Card por el botón de selección de hotel
 _buildHotelSelectionButton(context),
+SizedBox(height: 26),
+             Card(
+  child: DottedBorder(
+    dashPattern: [8, 4], // Esto define el patrón del punto y el espacio
+    strokeWidth: 2, // Esto define el grosor de la línea
+    color: Colors.grey, // Esto define el color de la línea punteada
+    child: Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: RichText(
+        text: TextSpan(
+          children: [
+            TextSpan(
+              text: 'Total: ',
+              style: TextStyle(
+                fontSize: 24.0,
+                color: Colors.black,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            TextSpan(
+              text: '\$${total.toStringAsFixed(2)} ',
+              style: TextStyle(
+                fontSize: 24.0,
+                color: Colors.green[700], // Un verde oscuro para el precio
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            TextSpan(
+              text: 'MXN',
+              style: TextStyle(
+                fontSize: 18.0,
+                color: Colors.black54,
+                fontWeight: FontWeight.normal,
+              ),
+            ),
+          ],
+        ),
+      ),
+    ),
+  ),
+),
+SizedBox(height: 26),
 
-                Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: Text('Total: \$${total.toStringAsFixed(2)} MXN'),
-                ),
-                ElevatedButton(
-                  onPressed: () {
-                    // Implementar lógica para guardar la reserva
-                  },
-                  child: Text('Confirmar Reserva'),
-                ),
+               Align(
+                        alignment: Alignment.center,
+                        child: Padding(
+                          padding: const EdgeInsets.all(18.0),
+                          child: ElevatedButton(
+                            onPressed: () {
+                              Navigator.of(context).push(
+                                MaterialPageRoute(
+                                  builder: (context) => ConfirmationScreen(
+                                     
+                                    title: widget.title, 
+                                    packages: widget.packages,
+                                  
+                                  ),
+                                ),
+                              );
+                            },
+                            child: Text(
+                              'Reservar ahora',
+                              style: TextStyle(
+                                fontSize: 20,
+                                color: Colors.white,
+                              ),
+                            ),
+                            style: ElevatedButton.styleFrom(
+                              minimumSize: Size(double.infinity, 50),
+                              primary: Theme.of(context).primaryColor,
+                            ),
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 30),
               ],
             ),
           ),
